2ead2b3e6c1c9e950831df3a47f3afba1f467963
nox <93527521+kaique012@users.noreply.github.com>
2026-05-14 18:57:15 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiAppHotel;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        // NÃO COLOQUE "MainPage = ..." AQUI!
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        // Aqui definimos a página inicial corretamente
+        var navegacao = new NavigationPage(new Views.ContratacaoHospedagem());
+
+        return new Window(navegacao)
+        {
+            Width = 400,
+            Height = 600
+        };
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiAppHotel
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("Oswald-Bold.ttf", "Oswald-Bold");
+                    fonts.AddFont("Oswald-ExtraLight.ttf", "Oswald-ExtraLight");
+                    fonts.AddFont("Oswald-Light.ttf", "Oswald-Light");
+                    fonts.AddFont("Oswald-Medium.ttf", "Oswald-Medium");
+                    fonts.AddFont("Oswald-Regular.ttf", "Oswald-Regular");
+                    fonts.AddFont("Oswald-SemiBold.ttf", "Oswald-SemiBold"); 
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}

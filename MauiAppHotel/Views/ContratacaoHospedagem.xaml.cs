044c8fb7d359fe52c0161c00a09afd8fc99bdf8d
kaique nunes <knunes070@gmail.com>
2026-05-14 18:11:29 -0300

    MauiAppHotel

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -6,4 +6,16 @@ public partial class ContratacaoHospedagem : ContentPage
     {
         InitializeComponent();
     }
-}
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                // Navega para a tela Sobre (ou a tela de Resumo que você criar)
+                await Navigation.PushAsync(new SobreView());
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+    }
